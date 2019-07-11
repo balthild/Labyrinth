db0cb5eef298f8da4c89f302c825231347fef3b3
Balthild <ibalthild@gmail.com>
2019-07-11 15:47:40 +0800

    Fix build script

--- build-clash.sh
+++ build-clash.sh
@@ -5,16 +5,16 @@ rm -rf ./dist/bin/clash-*
 pushd clash
 
 echo 'Building Clash for GNU/Linux 64 Bit'
-GOOS=linux GOARCH=amd64 go build -o ../dist/bin/clash-linux-x64 github.com/Dreamacro/clash
+GOOS=linux GOARCH=amd64 go build -o ../build/bin/clash-linux-x64 github.com/Dreamacro/clash
 echo 'Building Clash for GNU/Linux 32 Bit'
-GOOS=linux GOARCH=386 go build -o ../dist/bin/clash-linux-x32 github.com/Dreamacro/clash
+GOOS=linux GOARCH=386 go build -o ../build/bin/clash-linux-x32 github.com/Dreamacro/clash
 
 echo 'Building Clash for macOS 64 Bit'
-GOOS=darwin GOARCH=amd64 go build -o ../dist/bin/clash-darwin-x64 github.com/Dreamacro/clash
+GOOS=darwin GOARCH=amd64 go build -o ../build/bin/clash-darwin-x64 github.com/Dreamacro/clash
 
 echo 'Building Clash for Windows 64 Bit'
-GOOS=windows GOARCH=amd64 go build -o ../dist/bin/clash-win32-x64.exe github.com/Dreamacro/clash
+GOOS=windows GOARCH=amd64 go build -o ../build/bin/clash-win32-x64.exe github.com/Dreamacro/clash
 echo 'Building Clash for Windows 32 Bit'
-GOOS=windows GOARCH=386 go build -o ../dist/bin/clash-win32-x32.exe github.com/Dreamacro/clash
+GOOS=windows GOARCH=386 go build -o ../build/bin/clash-win32-x32.exe github.com/Dreamacro/clash
 
 popd
