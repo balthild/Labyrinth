4321b349b2d144383428762ad556da8b2919db6f
Balthild <ibalthild@gmail.com>
2019-08-31 03:05:29 +0800

    Switching proxy selectors

---
+++ src/renderer/components/DetailTitle.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './DetailTitle.scss';
+
+type DetailTitleProps = {
+    onBackButtonClick?(e: React.MouseEvent<HTMLDivElement>): void;
+};
+
+const DetailTitle: React.FC<React.PropsWithChildren<DetailTitleProps>> = (props) => (
+    <div className="detail-title">
+        <div className="back-btn" onClick={props.onBackButtonClick}>
+            <i className="ion ion-ios-arrow-back" />
+        </div>
+
+        <h2>{props.children}</h2>
+    </div>
+);
+
+export default DetailTitle;
--- src/renderer/pages/Proxy.tsx
+++ src/renderer/pages/Proxy.tsx
@@ -3,27 +3,57 @@ import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 
 import './Proxy.scss';
-import { Config } from '@/types/Config';
-import { Group, Proxies, ProxyItem } from '@/types/ClashProxy';
+import { Group, Proxies, Proxy, ProxyItem, Selector } from '@/types/ClashProxy';
 import { ClashConfig } from '@/types/ClashConfig';
-import { getConfigFilePath } from '@/util';
-import { Action, ActionTypes, GlobalState } from '@/renderer/store';
+import { Action, GlobalState } from '@/renderer/store';
 import { getControllerUrl } from '@/renderer/util';
-import { writeAppConfig } from '@/renderer/config';
 import PageTitle from '@/renderer/components/PageTitle';
+import DetailTitle from '@/renderer/components/DetailTitle';
 
 type ProfileProps = {
     config: ClashConfig;
-    appConfig: Config;
 };
 
 type ProfileState = {
     proxies: Proxies;
+    showDetail: boolean;
+    detailName: string;
 };
 
 class Profile extends React.Component<ProfileProps, ProfileState> {
     state = {
-        proxies: {},
+        proxies: {} as Proxies,
+        showDetail: false,
+        detailName: '',
+    };
+
+    showDetail = (e: React.MouseEvent<HTMLLIElement>) => {
+        const name = e.currentTarget.dataset.name!!;
+
+        this.setState({
+            showDetail: true,
+            detailName: name,
+        });
+    };
+
+    backToList = (e: React.MouseEvent) => {
+        this.setState({
+            showDetail: false,
+        });
+    };
+
+    setSelectorProxy = async (e: React.MouseEvent<HTMLElement>) => {
+        const { selector, proxy } = e.currentTarget.dataset;
+
+        const proxiesUrl = getControllerUrl(`/proxies/${selector}`);
+        await fetch(proxiesUrl, {
+            method: 'PUT',
+            body: JSON.stringify({
+                name: proxy,
+            }),
+        });
+
+        await this.loadProxies();
     };
 
     async loadProxies() {
@@ -34,16 +64,14 @@ class Profile extends React.Component<ProfileProps, ProfileState> {
         });
     }
 
-    getProxiesShouldDisplay(): [string, Group][] {
-        const groupTypes = ['URLTest', 'Fallback', 'LoadBalance', 'Selector'];
+    getProxiesShouldDisplay() {
         const showGlobal = this.props.config.mode === 'Global';
-
         const orders = {
             'GLOBAL': 1,
             'Proxy': 2,
         };
 
-        return Object.entries<Group>(this.state.proxies)
+        const groupEntries = Object.entries<ProxyItem>(this.state.proxies)
             .filter(([name, proxy]) => {
                 return name === 'GLOBAL' ? showGlobal : groupTypes.includes(proxy.type);
             })
@@ -53,41 +81,113 @@ class Profile extends React.Component<ProfileProps, ProfileState> {
 
                 return aOrder - bOrder;
             });
+
+        return groupEntries as [string, Group][];
     }
 
     componentDidMount() {
         this.loadProxies();
     }
 
-    render() {
-        const current = this.props.appConfig.configFile;
+    renderGroupDetail(groupName: string, group?: Group) {
+        if (!group) {
+            return null;
+        }
+
+        const { proxies } = this.state;
+
+        const isSelector = group.type === 'Selector';
 
         return (
-            <div className="proxy">
-                <PageTitle>Proxy</PageTitle>
+            <ul className="clash-proxies">
+                {group.all.map((proxyName) => {
+                    const proxy = proxies[proxyName];
+                    const isGroup = groupTypes.includes(proxy.type);
 
-                <ul className="clash-proxies">
-                    {this.getProxiesShouldDisplay().map(([name, proxy]: [string, Group]) => (
-                        <li key={name}>
+                    const selectorSwitcherProps = isSelector ? {
+                        'data-selector': groupName,
+                        'data-proxy': proxyName,
+                        onClick: this.setSelectorProxy,
+                    } : {};
+
+                    return (
+                        <li key={proxyName} className={proxyName === group.now ? 'current' : ''}>
                             <div>
-                                <span className="name">{name}</span>
-                                <span className="type">{proxy.type}</span>
+                                <span className="name">{proxyName}</span>
+                                <span className="type">{proxies[proxyName].type}</span>
                             </div>
                             <div className="description">
-                                <i className="ion ion-md-return-right" />
-                                {proxy.now}
+                                {isGroup ? (
+                                    <>
+                                        <i className="ion ion-md-return-right" />
+                                        {(proxy as Group).now}
+                                    </>
+                                ) : (proxy as Proxy).history.slice(0, 3).map((h) => (
+                                    <span key={h.time} className="delay">{h.delay}ms</span>
+                                ))}
                             </div>
+
+                            {isSelector && (
+                                <div className="active-sign" {...selectorSwitcherProps}>
+                                    <i className={`ion ${activeSignIconClass(proxyName, group.now)}`} />
+                                </div>
+                            )}
                         </li>
-                    ))}
-                </ul>
-            </div>
+                    );
+                })}
+            </ul>
+        );
+    }
+
+    render() {
+        const { showDetail, detailName, proxies } = this.state;
+
+        const proxyListClass = showDetail ? 'showing-detail' : '';
+        const detailClass = showDetail ? 'open' : '';
+
+        const detailItem = proxies[detailName] as Group;
+
+        return (
+            <>
+                <div className={`proxy ${proxyListClass}`}>
+                    <PageTitle>Proxy</PageTitle>
+
+                    <ul className="clash-proxies">
+                        {this.getProxiesShouldDisplay().map(([name, proxy]) => (
+                            <li key={name} data-name={name} onClick={this.showDetail}>
+                                <div>
+                                    <span className="name">{name}</span>
+                                    <span className="type">{proxy.type}</span>
+                                </div>
+                                <div className="description">
+                                    <i className="ion ion-md-return-right" />
+                                    {proxy.now}
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className={`proxy-detail-shade ${detailClass}`} />
+                <div className={`proxy-detail ${detailClass}`}>
+                    <DetailTitle onBackButtonClick={this.backToList}>{detailName}</DetailTitle>
+
+                    {this.renderGroupDetail(detailName, detailItem)}
+                </div>
+            </>
         );
     }
 }
 
+const groupTypes = ['URLTest', 'Fallback', 'LoadBalance', 'Selector'];
+
+const activeSignIconClass = (config: string, currentConfig: string) =>
+    config === currentConfig ?
+        'ion-ios-checkmark-circle' :
+        'ion-ios-checkmark-circle-outline';
+
 const mapStateToProps = (state: GlobalState) => ({
     config: state.config,
-    appConfig: state.appConfig,
 });
 
 const mapDispatchToProps = (dispatch: Dispatch<Action>) => ({});
